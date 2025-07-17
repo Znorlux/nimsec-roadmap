86e22ddf51566581f7277b76a665c074b675197c
Znorlux <christianlombardi0414@gmail.com>
2025-07-17 18:23:04 -0500

    Version basica y funcional de la calculadora

--- calculator_cli/calc.nim
+++ calculator_cli/calc.nim
@@ -6,24 +6,46 @@ import strutils, strformat
   ... and ending with ]#
 ]#
 # Paso 1: Pide dos números y una operación
-let operaciones = @["Suma", "Resta", "Multiplicación", "División"]
+import strformat, strutils
+
+const operaciones = @["Suma", "Resta", "Multiplicación", "División"]
+
 stdout.write("Ingresa el primer número: ")
 let num1str = stdin.readLine()
 if num1str.allCharsInSet({'0'..'9'}): # si todos los caracteres de una cadena están dentro del conjunto (Equivale a: {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'})
-    let num1 = num1str.parseInt()
-else:
-    echo "Error, debes ingresar un número válido"
-    system.quit(0)
-stdout.write("Ingresa el segundo número: ")
-let num2str = stdin.readLine()
-if num2str.allCharsInSet({'0'..'9'}):
+  let num1 = num1str.parseInt()
+  stdout.write("Ingresa el segundo número: ")
+  let num2str = stdin.readLine()
+  if num2str.allCharsInSet({'0'..'9'}):
     let num2 = num2str.parseInt()
-else:
-    echo "Error, debes ingresar un número válido"
-    system.quit(0)
-echo "Las operaciones disponibles son las siguientes:"
-for i, op in operaciones:
-    echo fmt"{i + 1}.{op}" # fmt need strformat
 
-# Paso 2: Lógica de operaciones
-# Paso 3: Imprime el resultado
+    echo "Las operaciones disponibles son las siguientes:"
+    for i, op in operaciones:
+      echo fmt"{i + 1}. {op}"
+
+    stdout.write("Ingresa el número de la operación que quieres realizar (ej. 1 para suma): ")
+    let operacionStr = stdin.readLine()
+    if operacionStr.allCharsInSet({'1'..'4'}):
+      let operacion = operacionStr.parseInt()
+
+      case operacion:
+        of 1: echo fmt"Resultado: {num1} + {num2} = {num1 + num2}"
+        of 2: echo fmt"Resultado: {num1} - {num2} = {num1 - num2}"
+        of 3: echo fmt"Resultado: {num1} * {num2} = {num1 * num2}"
+        of 4:
+          if num2 == 0:
+            echo "Error: División por cero"
+          else:
+            echo fmt"Resultado: {num1} / {num2} = {num1 / num2}"
+        else:
+          echo "Opción inválida"
+    else:
+      echo "Opción inválida. Debes ingresar un número del 1 al 4."
+  else:
+    echo "Error, debes ingresar un número válido"
+else:
+  echo "Error, debes ingresar un número válido"
+
+        
+
+
